9d7cd54065c27142619eee62f20c691d6965636e
Ice-Citron <shng2025@gmail.com>
2024-01-24 14:38:23 +0800

    Finished part 1 introductory.

---
+++ Part 1 - Initial/index.js
@@ -0,0 +1,18 @@
+// console - right click inspect, and go to console, this is simillar to debug log. 
+//console.log(`Hello world.`);
+//console.log(`I like pizza!`); 
+
+// this creates a pop-up alert. 
+//window.alert(`This is an alert!`);
+//window.alert(`I like pizza!`);
+
+// getElementById() inputs id of certain element created in html, and then set its text content?
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`;
+
+// This is a comment
+
+/**
+    multi-line comments 
+    frfr
+ */
